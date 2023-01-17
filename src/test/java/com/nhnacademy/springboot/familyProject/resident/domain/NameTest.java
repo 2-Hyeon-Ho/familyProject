fabd6c6fb03e3d28645aa7f1b69d16aef6c2e2f1
2HyeonHo <dpqlvkvk@naver.com>
2023-01-17 18:22:59 +0900

    test: 이름 도메인 테스트 작성

---
+++ src/test/java/com/nhnacademy/springboot/familyProject/resident/domain/NameTest.java
@@ -0,0 +1,21 @@
+package com.nhnacademy.springboot.familyProject.resident.domain;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+class NameTest {
+
+    @DisplayName("이름이 빈 값이면 에러가 발생한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void validateNameNotEmpty(String name) {
+        assertThatThrownBy(() -> new Name(name))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("이름은 빈값이 허용되지 않습니다");
+    }
+
+}
