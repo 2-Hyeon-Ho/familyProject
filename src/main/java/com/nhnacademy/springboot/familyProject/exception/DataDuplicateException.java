4c5aab94498bc5ace434de11083f538d9d685239
2HyeonHo <dpqlvkvk@naver.com>
2023-01-22 18:52:38 +0900

    refactor: 커스텀에러 에러 메세지 변경

--- src/main/java/com/nhnacademy/springboot/familyProject/exception/DataDuplicateException.java
+++ src/main/java/com/nhnacademy/springboot/familyProject/exception/DataDuplicateException.java
@@ -2,6 +2,6 @@ package com.nhnacademy.springboot.familyProject.exception;
 
 public class DataDuplicateException extends RuntimeException {
     public DataDuplicateException(String data) {
-        super(data + " is exist!");
+        super(data + "이/가 이미 존재합니다!");
     }
 }
