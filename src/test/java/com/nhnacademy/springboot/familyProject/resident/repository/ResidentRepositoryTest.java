b8c68de066473d235725b9dce519b52f938cb500
2HyeonHo <dpqlvkvk@naver.com>
2023-01-22 13:01:02 +0900

    test: 주민 Repository테스트 작성

--- src/test/java/com/nhnacademy/springboot/familyProject/resident/repository/ResidentRepositoryTest.java
+++ src/test/java/com/nhnacademy/springboot/familyProject/resident/repository/ResidentRepositoryTest.java
@@ -1,14 +1,20 @@
-package com.nhnacademy.springboot.familyProject.repository.resident;
+package com.nhnacademy.springboot.familyProject.resident.repository;
 
+
+import com.nhnacademy.springboot.familyProject.common.constant.ErrorCode;
+import com.nhnacademy.springboot.familyProject.exception.CustomException;
+import com.nhnacademy.springboot.familyProject.resident.domain.Identification;
 import com.nhnacademy.springboot.familyProject.resident.domain.Resident;
-import com.nhnacademy.springboot.familyProject.exception.ResidentNotFoundException;
-import com.nhnacademy.springboot.familyProject.resident.repository.ResidentRepository;
+import com.nhnacademy.springboot.familyProject.resident.domain.ResidentRegistrationNumber;
+import java.util.Optional;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -24,17 +30,20 @@ class ResidentRepositoryTest {
         Integer id = 4;
 
         //when
-        Resident resident = residentRepository.findById(id).orElseThrow(ResidentNotFoundException::new);
+        Optional<Resident> resident = residentRepository.findById(id);
+        if(resident.isEmpty()) {
+            throw new CustomException(ErrorCode.RESIDENT_NOT_FOUND);
+        }
 
         //then
-        Assertions.assertThat(resident.getResidentId()).isEqualTo(id);
+        Assertions.assertThat(resident.get().getResidentId()).isEqualTo(id);
     }
 
     @Test
     @DisplayName("주민 ID로 주민 조회")
     void findByIdentification() {
         //given
-        String id = "resident4";
+        Identification id = new Identification("resident4");
 
         //when
         Resident actual = residentRepository.findById(id);
@@ -47,12 +56,26 @@ class ResidentRepositoryTest {
     @DisplayName("주민등록번호로 주민 조회")
     void findByResidentRegistrationNumber() {
         //given
-        String residentRegistrationNumber = "790510-1234564";
+        ResidentRegistrationNumber residentRegistrationNumber = new ResidentRegistrationNumber("790510-1234564");
 
         //when
         Resident actual = residentRepository.findByResidentRegistrationNumber(residentRegistrationNumber);
 
         //then
         Assertions.assertThat(actual.getResidentRegistrationNumber()).isEqualTo(residentRegistrationNumber);
+    }
+
+    @Test
+    @DisplayName("주민전체 조회")
+    void findAll() {
+        //given
+        int page = 0;
+        int size = 10;
+
+        //when
+        Page<Resident> actual = residentRepository.findAll(PageRequest.of(page, size));
+
+        //then
+        Assertions.assertThat(actual.getContent().size()).isEqualTo(actual.getTotalElements());
     }
 }
