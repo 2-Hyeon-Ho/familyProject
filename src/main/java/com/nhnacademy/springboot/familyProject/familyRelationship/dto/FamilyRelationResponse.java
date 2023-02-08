d2f8f50c1e77ccb0ca6bd26eda279051e0d5b7f2
2HyeonHo <dpqlvkvk@naver.com>
2023-02-08 18:50:16 +0900

    refactor: 가족관계증명서 Dto 패키지 위치 변경

--- src/main/java/com/nhnacademy/springboot/familyProject/familyRelationship/dto/FamilyRelationResponse.java
+++ src/main/java/com/nhnacademy/springboot/familyProject/familyRelationship/dto/FamilyRelationResponse.java
@@ -1,4 +1,4 @@
-package com.nhnacademy.springboot.familyProject.familyRelationshipCertificateIssue.dto;
+package com.nhnacademy.springboot.familyProject.familyRelationship.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
